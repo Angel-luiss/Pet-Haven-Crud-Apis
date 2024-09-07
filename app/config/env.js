e99bb5e283c98cf679dbccf248d5b15954712e9f
Angel Tucubal <angel@MacBook-Pro-de-Angel.local>
2024-09-06 20:40:20 -0600

    Primer_Commit1

---
+++ app/config/env.js
@@ -0,0 +1,17 @@
+
+
+const env = {
+  database: 'umg_antigua_web',
+  username: 'umg_antigua_web_user',
+  password: 'ELx6CLV9LXxyCI7gGFDwwUWz9ZG91CGw',
+  host: 'dpg-cqc7mst6l47c73cv9vc0-a',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+module.exports = env;

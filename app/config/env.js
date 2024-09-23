c37108006b0eb5ece5447c3b67e650172e875c90
Angel Tucubal <angel@MacBook-Pro-de-Angel.local>
2024-09-23 00:20:11 -0600

    Primer Commit Prueba Render

--- app/config/env.js
+++ app/config/env.js
@@ -1,15 +1,15 @@
 
 
 const env = {
-  database: 'umg_antigua_web',
+  database: 'umg_antigua_web_1x9j',
   username: 'umg_antigua_web_user',
-  password: 'ELx6CLV9LXxyCI7gGFDwwUWz9ZG91CGw',
-  host: 'dpg-cqc7mst6l47c73cv9vc0-a',
+  password: 'OXx23gu6Qbus7x2uQtV0t1d2lkUJzfQo',
+  host: 'dpg-crog3vo8fa8c738rajjg-a.oregon-postgres.render.com',   
   dialect: 'postgres',
   pool: {
     max: 5,
     min: 0,
-    acquire: 30000,
+    acquire: 30000, 
     idle: 10000
   }
 };
